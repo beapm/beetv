2bd32c75f9cc1a7d208a1f216470f8f0cdadd103
beapm <beapm1994@gmail.com>
2021-02-02 22:09:21 +0100

    Creación componentes: menu, home, login, logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './common/home/home.component';
+import { LoginComponent } from './common/login/login.component';
+import { LogoutComponent } from './common/logout/logout.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
